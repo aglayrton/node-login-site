9e9d3626089be73439b5283f01ec8df99c0453ec
Ju <aglayrtonjuliao@gmail.com>
2024-07-07 17:00:36 -0300

    Validação de token

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = {
+  eAdmin: async function validarToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Necessário enviar o token",
+      });
+    }
+
+    const token = authHeader.split(" ")[1]; // Pega apenas o token sem o "Bearer"
+
+    try {
+      const decoded = await jwt.verify(token, process.env.SECRET_KEY);
+
+      req.userId = decoded.id;
+      return next();
+    } catch (error) {
+      return res.status(400).json({
+        erro: true,
+        mensagem: "Token inválido",
+        token,
+      });
+    }
+  },
+};
