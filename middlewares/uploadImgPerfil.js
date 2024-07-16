2237f05a1d445ecefb69244e4b2c4724a829b8b3
Ju <aglayrtonjuliao@gmail.com>
2024-07-16 18:48:31 -0300

    Código do multer para upload de arquivos

---
+++ middlewares/uploadImgPerfil.js
@@ -0,0 +1,32 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+// // Caminho do diretório de upload
+// const uploadDir = path.join(__dirname, "./public/upload/usuarios");
+
+// // Cria o diretório se ele não existir
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
+
+module.exports = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./public/upload/usuarios");
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now().toString() + "_" + file.originalname);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    const extensaoImagem = ["image/png", "image/jpeg", "image/jpg"].find(
+      (formatoAceito) => formatoAceito === file.mimetype
+    );
+    if (extensaoImagem) {
+      cb(null, true);
+    } else {
+      cb(new Error("Tipo de arquivo não suportado"), false);
+    }
+  },
+});
